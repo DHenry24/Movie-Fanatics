426d18dee7fb0e5f1eded213945aa1209b5c38b2
Jay Madu <jmaduafokwa@hotmail.com>
2023-04-09 23:06:54 +0100

    fixed up CSS and added logic for search function to display detail page

--- Index.js
+++ Index.js
@@ -20,9 +20,11 @@ var mainSection = document.querySelector(".main-section");
 var homeSection = document.querySelector(".home-section");
 var searchEngine = document.querySelector(".search-engine");
 var searchGrid = document.querySelector(".search-grid");
+var movieDesc = document.querySelector(".movie-desc");
 
 document.querySelector(".img-btn").addEventListener("click", function() {
     searchEngine.classList.add("hidden");
+    movieDesc.classList.add("hidden");
     homeSection.classList.remove("hidden");
 })
 
@@ -39,12 +41,14 @@ mainInput.addEventListener("keypress", function(e) {
     if (e.key === "Enter") {
         searchEngine.classList.remove("hidden");
         homeSection.classList.add("hidden");
+        movieDesc.classList.add("hidden");
     }
 
     // Return to home page if there is no value
     if (value === "") {
-        searchEngine.classList.toggle("hidden");
-        homeSection.classList.toggle("hidden");
+        searchEngine.classList.add("hidden");
+        homeSection.classList.remove("hidden");
+        movieDesc.classList.add("hidden");
         mainInput.style.border = "none";
     }
 
@@ -52,6 +56,8 @@ mainInput.addEventListener("keypress", function(e) {
     fetchFilter(value.toLowerCase());    
 
 })
+
+
 
 
 // var privateKey = "80a597a0c93c43cf353b5afe4098352f521de240";
@@ -64,6 +70,19 @@ mainInput.addEventListener("keypress", function(e) {
 
 var omdbAPIKey = "d5fca4e1";
 
+var genre = document.querySelector(".genre");
+
+var descTitle = document.querySelector("#desc-title");
+var rating = document.querySelector("#rating");
+var rated = document.querySelector("#rated");
+var runtime = document.querySelector("#runtime");
+var year = document.querySelector("#year");
+var descParagraph = document.querySelector("#desc-paragraph");
+var director = document.querySelector("#director");
+var writer = document.querySelector("#writer");
+var actors = document.querySelector("#actors");
+var movieImage = document.querySelector("#movie-image");
+var languages = document.querySelector("#languages");
 
 const fetchFilter = async (value) => {
     let fetchOmdbUrl = "http://www.omdbapi.com/?apikey=" + omdbAPIKey + "&s=" + value + "&r=json&page=1";
@@ -97,41 +116,103 @@ const fetchFilter = async (value) => {
                     searchItem.appendChild(searchImage);
                     searchItem.appendChild(searchPara);
                     searchGrid.appendChild(searchItem);
+                    searchItem.style.cursor ="pointer";
                 }
+
+                searchItem.addEventListener("click", function() {
+                    descTitle.innerHTML = '';
+                    genre.innerHTML = '';
+                    rating.innerHTML = '';
+                    rated.innerHTML = '';
+                    runtime.innerHTML = '';
+                    year.innerHTML = '';
+                    descParagraph.innerHTML = '';
+                    director.innerHTML = '';
+                    writer.innerHTML = '';
+                    actors.innerHTML = '';
+                    movieImage.src = '';
+                    movieImage.alt = '';
+                    languages.innerHTML = '';
+
+                    fetchCard(this.lastChild.innerHTML);
+                    searchEngine.classList.add("hidden");
+                    homeSection.classList.add("hidden");
+                    movieDesc.classList.remove("hidden");
+
+                })
 
                 i++;
             }
 
-            // searchItem.addEventListener("click", function () {
-            //     console.log(this.dataset.id)
-            // })
         }
     } 
     catch(error) {
-        console.log(error);
+        searchGrid.innerHTML = `<p>No movies found.</p>`
     }
 }
  
 
-var search = "spiderman";
-var omdbAPIUrl = "http://www.omdbapi.com/?apikey=" + omdbAPIKey + "&s=" + search + "&r=json&page=2&type=movie"
+var search = "Wednesday";
 
-// OMDB API Fetch
-fetch(omdbAPIUrl)
+
+function fetchCard(value) {
+    // OMDB API Fetch
+    fetch("http://www.omdbapi.com/?apikey=" + omdbAPIKey + "&t=" + value + "&r=json&page=1&type=movie")
     .then(response => response.json())
-    .then(data => console.log(data));
+    .then(data => {
+        console.log(data);
+        descTitle.innerHTML = data.Title;
+        rating.innerHTML = data.imdbRating;
+        rated.innerHTML = data.Rated;
+        runtime.innerHTML = data.Runtime;
+        year.innerHTML = data.Year;
+        descParagraph.innerHTML = data.Plot;
+        director.innerHTML = data.Director;
+        writer.innerHTML = data.Writer;
+        actors.innerHTML = data.Actors;
+        movieImage.src = data.Poster;
+        movieImage.alt = data.Title;
+        languages.innerHTML = data.Language;
+
+        var genreSplit = data.Genre.split(", ") 
+        if (genreSplit.length > 1) {
+            for (let i = 0; i < genreSplit.length; i++) {
+                var genrePara = document.createElement("p");
+                var genreContainer = document.createElement("div");
+    
+                genrePara.innerHTML = genreSplit[i];
+                genreContainer.setAttribute("class", "genre-container");
+                genreContainer.appendChild(genrePara);
+                genre.appendChild(genreContainer);
+                genre.style.justifyContent = "space-evenly";
+    
+            }
+        } else if (genreSplit.length === 1) {
+            var genrePara = document.createElement("p");
+            var genreContainer = document.createElement("div");
+
+            genrePara.innerHTML = genreSplit[0];
+            genreContainer.setAttribute("class", "genre-container");
+            genreContainer.appendChild(genrePara);
+            genre.appendChild(genreContainer);
+            genre.style.justifyContent = "left";
+        }
+        
+    });
+}
+
 
     
 
 const swiperWrapper = document.querySelector('.swiper-wrapper');
 
-for (let i = 0; i < movies.length; i++) {
-    const movie = movies[i];
-    const slide = document.createElement('div');
-    slide.classList.add('swiper-slide');
-    slide.innerHTML = `
-          <img src="${movie.image}" alt="${movie.title}">
-          <h3>${movie.title}</h3>
-        `;
-    swiperWrapper.appendChild(slide);
-}
+// for (let i = 0; i < movies.length; i++) {
+//     const movie = movies[i];
+//     const slide = document.createElement('div');
+//     slide.classList.add('swiper-slide');
+//     slide.innerHTML = `
+//           <img src="${movie.image}" alt="${movie.title}">
+//           <h3>${movie.title}</h3>
+//         `;
+//     swiperWrapper.appendChild(slide);
+// }
